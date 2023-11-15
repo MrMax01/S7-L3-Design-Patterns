ba19dc1db2774679dc1b10bd2d568362f1ef8cff
MrMax01 <massimo.mauro00@gmail.com>
2023-11-15 16:09:23 +0100

    first commit

---
+++ src/main/java/massimomauro/S7L3DesignPatterns/S7L3DesignPatternsApplication.java
@@ -0,0 +1,13 @@
+package massimomauro.S7L3DesignPatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S7L3DesignPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S7L3DesignPatternsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/massimomauro/S7L3DesignPatterns/S7L3DesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package massimomauro.S7L3DesignPatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S7L3DesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
